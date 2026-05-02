9aec9f71511bebaaa2a5be54dbaf7ff300030d86
owie-ow-ow <01100010ox@gmail.com>
2026-05-02 12:43:11 -0400

    Update files

--- script.js
+++ script.js
@@ -8,11 +8,12 @@ var searchSubmit = document.getElementById('searchsubmit')
 var nameText = document.getElementById('locNameTxt')
 var searchResultsModal = document.getElementById('resultsModal')
 var locatingMessage = document.getElementById('locatingMessage')
-var asOfText = document.getElementById('asOfText')
+
+const styleSheet = document.getElementById('styleSheet')
 
 const currentWeatherBase = "https://api.msn.com/weatherfalcon/weather/current?apikey=j5i4gDqHL6nGYwx5wi5kRhXjtf2c5qgFX9fzfk0TOo&activityId=69c11dee-40e6-4888-94da-3a88be97002d&cm=en-us&it=web&user=m-2361EA89BB366C582EEFFDADBA956D7F&scn=ANON&locale=en-us&units=F&appId=9e21380c-ff19-4c78-b4ea-19558e93a5d3&wrapOData=true"
 const search = "https://www.bing.com/api/v6/Places/AutoSuggest?appid=EDEC3CB74CF190BBBE26DF7938F3D961E925F593&types=Place&count=10&structuredaddress=true&strucaddrread=1&q="
-const hourlyWeatherBase = "https://api.msn.com/weather/hourlyforecast?apiKey=j5i4gDqHL6nGYwx5wi5kRhXjtf2c5qgFX9fzfk0TOo&appid=9e21380c-ff19-4c78-b4ea-19558e93a5d3&cm=en-US&locale=en&units=F&days=1"
+const hourlyWeatherBase = "https://api.msn.com/weather/hourlyforecast?apiKey=j5i4gDqHL6nGYwx5wi5kRhXjtf2c5qgFX9fzfk0TOo&appid=9e21380c-ff19-4c78-b4ea-19558e93a5d3&cm=en-US&locale=en&units=F&days=12"
 var lat = undefined
 var lon = undefined
 
@@ -153,6 +154,13 @@ async function getWeather(lat, lon) {
     nameText.textContent = locName
 
     document.body.style.backgroundImage = `url(backgrounds/${await mapCode(weather_code, daytime)}.jpg)`
+
+    var items = document.getElementsByClassName('subCardItem'), len = items !== null ? items.length : 0, i = 0;
+
+    for (i; i < len; i++) {
+        items[0].remove()
+    }
+    getDailyWeather(lat, lon)
     getHourlyWeather(lat, lon)
 }
 
@@ -167,22 +175,16 @@ async function getHourlyWeather(lat, lon) {
     response = await fetch(hourlyWeatherBase + `&lat=${lat}&lon=${lon}`)
     json = await response.json()
 
-    var items = document.getElementsByClassName('hourlyItem'), len = items !== null ? items.length : 0, i = 0;
-
-    for (i; i < len; i++) {
-        items[0].remove()
-    }
-
     current = json['value'][0]['responses'][0]['weather'][0]['days'][0]['hourly'].forEach(element => {
-        card = document.getElementById('hourlycard')
+        card = document.getElementById('hourlyCard')
 
         container = document.createElement('div')
 
         var stamp = element['valid'];
 
-        var now = new Date().toLocaleString('en-US', { hour: '2-digit', minute: '2-digit', hour12: true });
+        var now = new Date().toLocaleString(navigator.language, { hour: '2-digit', minute: '2-digit', hour12: true });
 
-        asOfText.textContent = "Hourly weather as of " + now;
+        document.getElementById('asOfText').textContent = "Hourly weather as of " + now;
 
         img = element['urlIcon']
         cap = element['cap']
@@ -191,12 +193,12 @@ async function getHourlyWeather(lat, lon) {
         capElement.textContent = cap
 
         var tempText = document.createElement('p')
-        tempText.classList.add('hourlyTemp')
+        tempText.classList.add('cardTemp')
         tempText.textContent = `${element['temp']}${json['value'][0]['units']['temperature']}`
         container.appendChild(tempText)
 
         var hourElement = document.createElement('h6')
-        hourElement.classList.add('hourlyText')
+        hourElement.classList.add('cardText')
         hourElement.textContent = fmtDate(new Date(stamp))
 
         var imgElement = document.createElement('img')
@@ -206,15 +208,73 @@ async function getHourlyWeather(lat, lon) {
         container.appendChild(capElement)
         tempText.appendChild(hourElement)
 
-        container.classList.add('hourlyItem')
+        container.classList.add('subCardItem')
         container.classList.add('border')
 
         card.appendChild(container)
     });
 }
 
+async function getDailyWeather(lat, lon) {
+    response = await fetch(hourlyWeatherBase + `&lat=${lat}&lon=${lon}`)
+    json = await response.json()
+
+    var card = document.getElementById('dailyCard')
+    current = json['value'][0]['responses'][0]['weather'][0]['days'].forEach(element => {
+        let curHour = element['hourly'][0]
+        console.log(curHour)
+
+        container = document.createElement('div')
+
+        var stamp = curHour['valid'];
+
+        var now = new Date().getTime() + 24 * 60 * 60 * 1000
+
+        document.getElementById('asOfTextDaily').textContent = "Daily weather as of " + new Date().toLocaleString(navigator.language, { "weekday": "long" });
+        
+        img = curHour['urlIcon']
+        cap = curHour['cap']
+        summary = curHour['summary']
+        
+        var capElement = document.createElement('p')
+        capElement.textContent = cap
+        var tempText = document.createElement('p')
+        tempText.classList.add('cardTemp')
+        tempText.textContent = `${curHour['temp']}${json['value'][0]['units']['temperature']}`
+        container.appendChild(tempText)
+
+        var dayElement = document.createElement('h6')
+        dayElement.classList.add('cardText')
+        if (new Date().getDate() == new Date(stamp).getDate()) {
+            dayElement.textContent = 'Today'
+        }
+        else {
+            dayElement.textContent = new Date(stamp).toLocaleString(navigator.language, { "weekday": "long" })
+        }
+
+        var dateElement = document.createElement('h6')
+        dateElement.classList.add('cardText')
+        dateElement.textContent = new Date(stamp).toLocaleString(navigator.language, { "dateStyle": "long" })
+
+        var imgElement = document.createElement('img')
+        imgElement.src = img
+
+        container.appendChild(imgElement)
+        container.appendChild(capElement)
+        tempText.appendChild(dayElement)
+        tempText.appendChild(dateElement)
+
+        container.classList.add('subCardItem')
+        container.classList.add('border')
+
+        console.log(container)
+
+        card.appendChild(container)
+    });
+}
+
 function fmtDate(date) {
-    return date.toLocaleString('en-US', { hour: 'numeric', hour12: true })
+    return date.toLocaleString(navigator.language, { hour: 'numeric', hour12: true })
 }
 
 navigator.geolocation.getCurrentPosition(async (position) => {
